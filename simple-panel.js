dac14d3bd4166b194cdc52b12014ef2397251328
Alexander Schmitz <arschmitz@gmail.com>
2014-05-08 14:01:40 -0400

    Add Reveal mode

--- simple-panel.js
+++ simple-panel.js
@@ -11,11 +11,12 @@ $.widget( "mobile.simplepanel", {
 	options: {
 		"size": 280,
 		"threshold": 100,
-		"handle": true,
+		"handle": null,
 		"enhanced": false,
 		"position": "left",
 		"theme": "a",
-		"scroll": false
+		"scroll": false,
+		"mode": "overlay"
 	},
 	_create: function(){
 		if( !this.options.enhanced ) {
@@ -43,60 +44,98 @@ $.widget( "mobile.simplepanel", {
 		});
 	},
 	_refresh: function(){
-		var axis = "x",
-			windowWidth = this.window.width(),
-			windowHeight = this.element.parent().css("position", "relative").outerHeight(),
-			handleProps = {},
-			cursor = {},
-			cssProps = {
+		this.axis = "x";
+		this.windowWidth = this.window.width();
+		this.handleProps = {};
+		this.cssProps = {
 				width: this.options.size,
-				height: windowHeight,
-			},
-			containment = [ 0, 0, 0, 0 ];
+				height: this.window.height(),
+			};
+		this.windowWidth = this.window.width();
 
-		switch ( this.options.position ) {
-			case "left":
-				containment[0] =  -( this.options.size );
-				cssProps.left = -( this.options.size );
-				cssProps.top = 0;
-				handleProps.top = 0;
-				handleProps.right = "-52px";
-				this.openPosition = 0;
-				this.closePosition = -( this.options.size );
-				break;
-			case "right":
-				containment[0] = windowWidth - this.options.size;
-				containment[2] =  windowWidth;
-				cssProps.left = windowWidth;
-				handleProps.top = 0;
-				handleProps.left = "-52px";
-				cssProps.top = 0;
-				this.openPosition = windowWidth - this.options.size;
-				this.closePosition = windowWidth;
-				break;
-			case "top":
-				containment[1] = -( this.options.size );
-				cssProps.left = 0;
-				cssProps.top = -( this.options.size );
-				cssProps.width = "100%";
-				cssProps.height = this.options.size + "px";
-				handleProps.bottom = "-52px";
-				handleProps.left = "50%";
-				handleProps[ "margin-left" ] = "-26px";
-				this.openPosition = 0;
-				this.closePosition = -( this.options.size );
-				axis = "y";
-				break;
-		}
-		this.element.css( cssProps );
-		this.element.draggable({
-			"axis": axis,
+		this.containment = [ 0, 0, 0, 0 ];
+
+		this[ "_" + this.options.mode + this.options.position ]();
+		this.element.css( this.cssProps );
+		this.dragElement.draggable({
+			"axis": this.axis,
 			"stop": this._dragStop,
-			"cursorAt": cursor,
-			"containment": containment,
+			"containment": this.containment,
 			"handle": this.handle
-		});
-		this.handle.css( handleProps );
+		}).data( "simplepanelref", this.element );
+		this.handle.css( this.handleProps ).appendTo( this.dragElement );
+	},
+	_overlaytop: function(){
+		this.dragElement = this.element;
+		this.containment[1] = -( this.options.size );
+		this.cssProps.left = 0;
+		this.cssProps.top = -( this.options.size );
+		this.cssProps.width = "100%";
+		this.cssProps.height = this.options.size + "px";
+		this.handleProps.bottom = "-52px";
+		this.handleProps.left = "50%";
+		this.handleProps[ "margin-left" ] = "-26px";
+		this.openPosition = 0;
+		this.closePosition = -( this.options.size );
+		this.axis = "y";
+	},
+	_overlayleft: function(){
+		this.dragElement = this.element;
+		this.containment[0] =  -( this.options.size );
+		this.cssProps.left = -( this.options.size );
+		this.cssProps.top = 0;
+		this.handleProps.top = 0;
+		this.handleProps.right = "-52px";
+		this.openPosition = 0;
+		this.closePosition = -( this.options.size );
+	},
+	_overlayright: function(){
+		this.dragElement = this.element;
+		this.containment[0] = this.windowWidth - this.options.size;
+		this.containment[2] =  this.windowWidth;
+		this.cssProps.left = this.windowWidth;
+		this.handleProps.top = 0;
+		this.handleProps.left = "-52px";
+		this.cssProps.top = 0;
+		this.openPosition = this.windowWidth - this.options.size;
+		this.closePosition = this.windowWidth;
+	},
+	_revealleft: function() {
+		this.dragElement = $( ".ui-page-active" );
+		this.containment[2] = this.options.size;
+		this.cssProps.left = 0;
+		this.cssProps.top = 0;
+		this.cssProps[ "z-index" ] = 0;
+		this.handleProps.left = 0;
+		this.handleProps.top = 0;
+		this.openPosition = this.options.size;
+		this.closePosition = 0;
+	},
+	_revealright: function() {
+		this.dragElement = $( ".ui-page-active" );
+		this.containment[0] = -(this.options.size);
+		this.cssProps.right = 0;
+		this.cssProps.top = 0;
+		this.cssProps[ "z-index" ] = 0;
+		this.handleProps.right = 0;
+		this.handleProps.top = 0;
+		this.openPosition = -(this.options.size);
+		this.closePosition = 0;
+	},
+	_revealtop: function() {
+		this.dragElement = $( ".ui-page-active" );
+		this.containment[3] = this.options.size;
+		this.cssProps.left = 0;
+		this.cssProps.top = 0;
+		this.cssProps.width = "100%";
+		this.cssProps.height = this.options.size + "px";
+		this.cssProps[ "z-index" ] = 0;
+		this.handleProps.top = "-8px";
+		this.handleProps.left = "50%";
+		this.handleProps[ "margin-left" ] = "-26px";
+		this.openPosition = this.options.size;
+		this.closePosition = 0;
+		this.axis = "y";
 	},
 	enhance: function(){
 		this.element
@@ -106,19 +145,23 @@ $.widget( "mobile.simplepanel", {
 				"height": this.window.height(),
 			}).addClass( "ui-simple-panel-scroll" );
 		}
-		this.handle = $( "<div>" )
-			.addClass("ui-btn ui-icon-bars ui-btn-icon-notext ui-simple-panel-handle")
-			.appendTo( this.element );
+		if ( !this.options.handle ) {
+			this.handle = $( "<div>" )
+			.addClass("ui-btn ui-icon-bars ui-btn-icon-notext ui-simple-panel-handle");
+		} else {
+			this.handle = $( this.options.handle );
+		}
 	},
 	_setOptions: function( options ){
 		return this._super( options );
 	},
 	_dragStop: function(){
 		var props = {},
-			widget = $( this ).data( "mobileSimplepanel" ),
+			widget = $( this ).data( "simplepanelref" ).data("mobileSimplepanel"),
 			options = widget.options,
 			direction = ( options.position === "left" || options.position === "right" ) ? "left" : "top",
 			offset = $( this ).offset();
+
 		if( offset[ direction ] !== widget.openPosition && offset[ direction ] !== widget.closePosition ) {
 			if( ( ( options.position === "left" || options.position === "top" ) &&
 				offset[ direction ] > ( widget.closePosition + ( options.size / 2 ) ) ) ||
@@ -128,7 +171,7 @@ $.widget( "mobile.simplepanel", {
 				offset[ direction ] < ( widget.closePosition - ( options.size / 2 ) ) )
 			){
 				props[ direction ] = widget.openPosition;
-				$( this ).animate( props,200);
+				$( this ).animate( props,200 );
 			} else {
 				props[ direction ] = widget.closePosition;
 				$( this ).animate(props,200);
